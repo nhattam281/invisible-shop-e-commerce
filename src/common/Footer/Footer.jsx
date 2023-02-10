45d52ab1dc1d437cdfce380b39d75fab662c88b9
Nhat Tam <nhattamlh281@gmail.com>
2023-02-10 22:58:23 +0700

    update and responsive footer

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -1,53 +1,70 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import './Footer.scss';
 
 function Footer() {
     return (
         <div className='footer'>
             <div className='footer_container'>
-                <div className='footer_container-col'>
-                    <h4>company</h4>
-                    <ul>
-                        <li>about us</li>
-                        <li>our services</li>
-                        <li>privacy policy</li>
-                        <li>affiliate program</li>
-                    </ul>
+                <div className='footer_logo'>
+                    <h1>invisibleShop.</h1>
                 </div>
-                <div className='footer_container-col'>
-                    <h4>Get help</h4>
-                    <ul>
-                        <li>FAQs</li>
-                        <li>Shipping</li>
-                        <li>Returns</li>
-                        <li>Payment Options</li>
-                    </ul>
-                </div>
-                <div className='footer_container-col'>
-                    <h4>online shop</h4>
-                    <ul>
-                        <li>smartphones</li>
-                        <li>laptops</li>
-                        <li>Dress</li>
-                        <li>Shoes</li>
-                    </ul>
-                </div>
-                <div className='footer_container-col'>
-                    <div className='footer_socials'>
-                        <h4>Follow Us</h4>
-                        <NavLink to='/' className='footer_button-social'>
-                            <i class='fab fa-facebook-f' />
-                        </NavLink>
-                        <NavLink to='/' className='footer_button-social'>
-                            <i class='fab fa-twitter' />
-                        </NavLink>
-                        <NavLink to='/' className='footer_button-social'>
-                            <i class='fab fa-instagram' />
-                        </NavLink>
-                        <NavLink to='/' className='footer_button-social'>
-                            <i class='fab fa-linkedin-in' />
-                        </NavLink>
+                <div className='footer_content'>
+                    <div className='footer_content-socials'>
+                        <h3>Follow us:</h3>
+                        <a
+                            href='http://facebook.com'
+                            className='footer_social-button'
+                        >
+                            <i className='fab fa-facebook-f' />
+                        </a>
+                        <a
+                            href='https://twitter.com/'
+                            className='footer_social-button'
+                        >
+                            <i className='fab fa-twitter' />
+                        </a>
+                        <a
+                            href='https://www.instagram.com/'
+                            className='footer_social-button'
+                        >
+                            <i className='fab fa-instagram' />
+                        </a>
+                        <a
+                            href='https://www.linkedin.com/'
+                            className='footer_social-button'
+                        >
+                            <i className='fab fa-linkedin-in' />
+                        </a>
+                    </div>
+
+                    <div className='footer_content-info'>
+                        <div className='footer_content-infor-items'>
+                            <h4>company</h4>
+                            <ul>
+                                <li>about us</li>
+                                <li>our services</li>
+                                <li>privacy policy</li>
+                                <li>affiliate program</li>
+                            </ul>
+                        </div>
+                        <div className='footer_content-infor-items'>
+                            <h4>Get help</h4>
+                            <ul>
+                                <li>FAQs</li>
+                                <li>Shipping</li>
+                                <li>Returns</li>
+                                <li>Payment Options</li>
+                            </ul>
+                        </div>
+                        <div className='footer_content-infor-items'>
+                            <h4>online shop</h4>
+                            <ul>
+                                <li>laptops</li>
+                                <li>Dress</li>
+                                <li>Shoes</li>
+                                <li>smartphones</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
